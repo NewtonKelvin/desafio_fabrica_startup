68d61059e6cf724771624c373deacf507b5ee4e4
Kelvin Newton <kelvinnewtonsm@gmail.com>
2023-04-04 22:14:48 -0300

    initial commit

---
+++ src/routes.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const routes = express.Router();
+
+//JWT AUTH
+import jwt from "jsonwebtoken";
+
+//Controllers
+// import loginController from "./controllers/login"
+
+export default routes;
